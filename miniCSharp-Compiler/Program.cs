98855ce8501d66bb9c447cf9cd3f12a26e13b488
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2020-08-19 16:06:32 -0600

    File reading

--- miniCSharp-Compiler/Program.cs
+++ miniCSharp-Compiler/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace miniCSharp_Compiler
@@ -10,6 +12,25 @@ namespace miniCSharp_Compiler
     {
         static void Main(string[] args)
         {
+
+            var path = "fileName.txt";//implement IDE for user
+            int column = 0;
+            int row = 0;
+
+            using (var sr = new StreamReader(path, Encoding.UTF8))
+            {
+                row++;
+                var fileLine = sr.ReadLine();
+                while (fileLine != null)
+                {
+                    row++;
+                    for (int i = 0; i < fileLine.Length; i++)
+                    {
+                        column++;
+                    }
+                    fileLine = sr.ReadLine();
+                }
+            }
         }
     }
 }
