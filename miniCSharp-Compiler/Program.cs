0715afa153ac4911506cf9f2bd2223128fae25ce
eduardotejedaleon <eduardotejedaleon@gmail.com>
2020-08-27 17:21:28 -0600

    English version

--- miniCSharp-Compiler/Program.cs
+++ miniCSharp-Compiler/Program.cs
@@ -10,10 +10,20 @@ namespace miniCSharp_Compiler
         {
             var fileExists = false;
             var path = string.Empty;
+            var englishVersion = false;
+            Console.WriteLine("Presiona cualquier tecla para la versión en español" + "\n");
+            Console.WriteLine("Press 'E' for english version");
+
+            if (Console.ReadKey().Key == ConsoleKey.E)
+            {
+                englishVersion = true;
+            }
+            var analyze = new LexicalAnalyzer(englishVersion);
+
             do
             {
                 Console.Clear();
-                Console.WriteLine("Ingrese la ruta al archivo que desea analizar");
+                Console.WriteLine(!englishVersion ? "Ingrese la ruta al archivo que desea analizar" : "Introduce your file path");
                 path = Console.ReadLine();
                 fileExists = File.Exists(path) ? true : false;
             } while (!fileExists);
@@ -21,27 +31,28 @@ namespace miniCSharp_Compiler
 
             var resultFilePath = "C:/lexicalAnalyzer/" + Path.GetFileNameWithoutExtension(path) + ".out";
 
-            var analyze = new LexicalAnalyzer();
             analyze.ReadFileAndAnalyzeDocument(path);
 
             analyze.PrintResultAndSaveToFile(resultFilePath);
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Tu archivo de salida se encuentra en " + resultFilePath);
+            Console.WriteLine(!englishVersion ? "Tu archivo de salida se encuentra en " : "You can find your output file in ");
+            Console.Write(resultFilePath);
 
             //printing area
             Console.WriteLine("\n");
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("¿Desea imprimir su documento? Presione 'y' para sí y otra tecla para no");
-            Console.WriteLine("Esto creará un archivo portable, su ubicación es: C:/lexicalAnalyzer" + Path.ChangeExtension(path, ".pdf"));
+            Console.WriteLine(!englishVersion ? "¿Desea imprimir su documento? Presione 'y' para sí y otra tecla para no" : "Would you like to print your file? Press 'y' for yes or any other key for no");
+            Console.WriteLine(!englishVersion ? "Esto creará un archivo portable, su ubicación es: " : "This will create a portable file that you can find in: ");
+            Console.Write("C:/lexicalAnalyzer" + Path.ChangeExtension(path, ".pdf" + "\n"));
 
             var printingResult = string.Empty;
             if (Console.ReadKey().Key == ConsoleKey.Y)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("\n");
-                Console.WriteLine("Asegúrate de que tu impresora esté conectada y encendida");
-                Console.WriteLine("Luego de haberte asegurado presiona enter :D");
+                Console.WriteLine(!englishVersion ? "Asegúrate de que tu impresora esté conectada y encendida" : "Make sure your printer is connected and turned on");
+                Console.WriteLine(!englishVersion ? "Luego de haberte asegurado presiona enter :D" : "After making sure, press enter :D");
                 Console.WriteLine("\n");
                 Console.ReadKey();
                 printingResult = analyze.PrintFile(resultFilePath);
@@ -51,15 +62,15 @@ namespace miniCSharp_Compiler
             var goodbyeMessage = string.Empty;
             if (DateTime.Now.Hour < 12)
             {
-                goodbyeMessage = "Ten un feliz día :D";
+                goodbyeMessage = !englishVersion ? "Ten un feliz día :D" : "Have a nice day :D";
             }
             else if (DateTime.Now.Hour < 18)
             {
-                goodbyeMessage = "Ten una feliz tarde :D";
+                goodbyeMessage = !englishVersion ? "Ten una feliz tarde :D" : "Have a nice afternoon :D";
             }
             else
             {
-                goodbyeMessage = "Ten una feliz noche :D";
+                goodbyeMessage = !englishVersion ? "Ten una feliz noche :D" : "Have a nice evening :D";
             }
 
             for (int i = 10; i >= 0; i--)
@@ -70,9 +81,8 @@ namespace miniCSharp_Compiler
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(goodbyeMessage + "\n");
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Esta consola se cerrará en ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(i.ToString());
+                Console.WriteLine(!englishVersion ? "Esta consola se cerrará en " : "This terminal will close in " + i.ToString()); Console.ForegroundColor = ConsoleColor.Green;
+
                 Thread.Sleep(1000);
             }
         }
