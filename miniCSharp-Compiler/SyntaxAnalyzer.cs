06c8cdfdf47c99206e2a3f5c2395ae29b60367b1
jetejeda <eduardotejedaleon@gmail.com>
2020-10-20 00:58:10 -0600

    Status backtracking, and code moduralization

--- miniCSharp-Compiler/SyntaxAnalyzer.cs
+++ miniCSharp-Compiler/SyntaxAnalyzer.cs
@@ -63,43 +63,67 @@ namespace miniCSharp_Compiler
                     analysisTableIns = conflicNode.Instructions[0];
 
                 }
-
-                if (analysisTableIns[0] == 's')
+                if (analysisTableIns == string.Empty)
                 {
-                    shiftTo(analysisTableIns, (isLexemeValue ? lexeme.Value : lexeme.Token.ToString()));
-
-                }
-                else if (analysisTableIns[0] == 'r')
-                {
-
-                    reduceBy(analysisTableIns);
-                    headerFound = Helper.GotoDict.TryGetValue(ConsumedSymbols.Peek(), out column);
-                    if (headerFound)
-                    {
-                        gotoStatus(column);
-                    }
+                    RestoreStatus(ref lexemesIndex, isLexemeValue, lexeme);
                 }
                 else
                 {
-                    //error
-                    if (ConflictsStack.Count == 0)
-                    {
-                        //Final error, not admitted by parser
-                    }
-                    else
-                    {
-                        //Restore to previous status before conflict
-                    }
+                    ShiftOrReduce(analysisTableIns, isLexemeValue, lexeme);
                 }
+
             }
             else
             {
-                //error
+                //Final error detected, unrecognized symbol
             }
 
 
         }
+        void RestoreStatus(ref int lexemesIndex, bool isLexemeValue, LexemeNode lexeme)
+        {
+            //error
+            if (ConflictsStack.Count == 0)
+            {
+                //Final error, input not admitted by parser
+            }
+            else
+            {
+                var previousStatus = ConflictsStack.Pop();
+                if (previousStatus.NextInstructionIndex < previousStatus.Instructions.Count())
+                {
+                    lexemesIndex = previousStatus.LexemesIndex;
+                    var analysisTableIns = previousStatus.Instructions[previousStatus.NextInstructionIndex];
+                    previousStatus.NextInstructionIndex++;
+                    ConflictsStack.Push(previousStatus);
+                    ShiftOrReduce(analysisTableIns, isLexemeValue, lexeme);
+                }
+                else
+                {
+                    //error, check the stack again
+                    RestoreStatus(ref lexemesIndex, isLexemeValue, lexeme);
+                }
+            }
+        }
+        void ShiftOrReduce(string analysisTableIns, bool isLexemeValue, LexemeNode lexeme)
+        {
+            if (analysisTableIns[0] == 's')
+            {
+                shiftTo(analysisTableIns, (isLexemeValue ? lexeme.Value : lexeme.Token.ToString()));
 
+            }
+            else if (analysisTableIns[0] == 'r')
+            {
+                var column = 0;
+                var headerFound = false;
+                reduceBy(analysisTableIns);
+                headerFound = Helper.GotoDict.TryGetValue(ConsumedSymbols.Peek(), out column);
+                if (headerFound)
+                {
+                    gotoStatus(column);
+                }
+            }
+        }
         string getAnalysisTableInstruction(int column)
         {
             return Helper.AnalysisTable[StatusStack.Peek() + 1, column];//to sync with table row index
