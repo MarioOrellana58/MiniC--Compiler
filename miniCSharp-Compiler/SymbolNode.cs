c33ec7e4a585e3d1162534e39dd89cb380011363
jetejeda <eduardotejedaleon@gmail.com>
2020-11-22 16:52:44 -0600

    Symbol node defined

---
+++ miniCSharp-Compiler/SymbolNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniCSharp_Compiler
+{
+    public class SymbolNode
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public dynamic Value { get; set; }
+        public int Scope { get; set; }
+        public List<string> Parameters { get; set; }
+        public int StartColumn { get; set; }
+        public int EndColumn { get; set; }
+        public int StartRow { get; set; }
+        public int EndRow { get; set; }
+        public SymbolNode()
+        {
+            Name = string.Empty;
+            Type = string.Empty;
+        }
+
+        public void InitParameters()
+        {
+            Parameters = new List<string>();
+        }
+
+    }
+}
